9eff57c081ce6b9f2f169b10c32230f7eddab7fa
Allen Russell <garyrusselljr@gmail.com>
2023-04-12 12:21:13 -0600

    Add variable to file

--- code_challenges/mild.rb
+++ code_challenges/mild.rb
@@ -3,3 +3,11 @@
 # Write a Ruby program that defines a variable that stores an Integer. 
 # The program should print out the String "even" if the Integer is even, 
 # and the String "odd" if the Integer is odd.
+
+number = 45
+
+if number % 2 == 0
+    puts "even"
+else
+    puts "odd"
+end
